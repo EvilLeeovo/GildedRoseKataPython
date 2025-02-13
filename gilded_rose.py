70ba239d7e7a89352318e2ebb662cfe802d79798
Jianchang Li <evillee0317@gmail.com>
2025-02-12 19:06:21 -0800

    update regularItemUpdater

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
 
 class Item:
@@ -11,7 +12,62 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+class ItemUpdater(ABC):
+    def __init__(self, item):
+        self.item = item
+    
+    @abstractmethod
+    def update(self):
+        pass
+    
+    def decrease_quality(self, amount=1):
+        if self.item.quality > 0:
+            self.item.quality = max(0, self.item.quality - amount)
 
+    def increase_quality(self, amount=1):
+        if self.item.quality < 50:
+            self.item.quality = min(50, self.item.quality + amount)
+
+    def decrease_sell_in(self):
+        self.item.sell_in -= 1
+
+class RegularItemUpdater(ItemUpdater):
+    def update(self):
+        self.decrease_quality(1)
+        self.decrease_sell_in()
+        if self.item.sell_in < 0:
+            self.decrease_quality(1)
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self):
+        pass
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self):
+        pass
+
+class BackstagePassUpdater(ItemUpdater):
+    def update(self):
+        pass
+
+class ConjuredUpdater(ItemUpdater):
+    def update(self):
+        pass
+
+class ItemUpdaterFactory:
+    @staticmethod
+    def get_updater(item):
+        if item.name == "Aged Brie":
+            return AgedBrieUpdater(item)
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            return SulfurasUpdater(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePassUpdater(item)
+        elif item.name.startswith("Conjured"):
+            return ConjuredUpdater(item)
+        else:
+            return RegularItemUpdater(item)
+    
 class GildedRose(object):
 
     def __init__(self, items: list[Item]):
@@ -20,30 +76,6 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            updater = ItemUpdaterFactory.get_updater(item)
+            updater.update()
+            
