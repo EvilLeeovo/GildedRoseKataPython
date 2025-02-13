dc61b96c3fcb00282c4d86e9cb1cb0eae5ef023a
Jianchang Li <evillee0317@gmail.com>
2025-02-12 21:35:28 -0800

    update test

--- gilded_rose.py
+++ gilded_rose.py
@@ -38,19 +38,15 @@ class RegularItemUpdater(ItemUpdater):
 
 class AgedBrieUpdater(ItemUpdater):
     def update(self):
-        if self.item.sell_in <= 0:
-            self.item.quality = 0
-        elif self.item.sell_in <= 5:
-            self.increase_quality(3)
-        elif self.item.sell_in <= 10:
-            self.increase_quality(2)
-        else:
-            self.increase_quality(1)
+        self.increase_quality(1)
         self.decrease_sell_in()
+        if self.item.sell_in < 0:
+            self.increase_quality(1)
 
 class SulfurasUpdater(ItemUpdater):
     def update(self):
-        pass
+        self.item.quality = 80
+        self.item.sell_in = self.item.sell_in
 
 class BackstagePassUpdater(ItemUpdater):
     def update(self):
